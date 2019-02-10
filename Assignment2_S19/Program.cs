93c0f5d33bbe45b5aed5d05cbfe98d3b585fbf15
arundhathi-patil <37865001+arundhathi-patil@users.noreply.github.com>
2019-02-10 16:38:53 -0500

    Changes made for question 3 and 4

--- Assignment2_S19/Program.cs
+++ Assignment2_S19/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace Assignment2_S19
 {
@@ -7,30 +9,32 @@ namespace Assignment2_S19
     {
         static void Main(string[] args)
         {
-            // left rotation
+            //left rotation
             Console.WriteLine("Left Rotation");
             int d = 4;
             int[] a = { 1, 2, 3, 4, 5 };
             int[] r = rotLeft(a, d);
             displayArray(r);
 
-            // Maximum toys
+            //Maximum toys
             Console.WriteLine("\n\nMaximum toys");
             int k = 50;
             int[] prices = { 1, 12, 5, 111, 200, 1000, 10 };
             Console.WriteLine(maximumToys(prices, k));
 
-            // Balanced sums
+            //Balanced sums
             Console.WriteLine("\n\nBalanced sums");
             List<int> arr = new List<int> { 1, 2, 3 };
             Console.WriteLine(balancedSums(arr));
+            Console.ReadKey();
 
-            // Missing numbers
+            //Missing numbers
             Console.WriteLine("\n\nMissing numbers");
-            int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206};
-            int[] brr = {203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204};
+            int[] arr1 = { 203, 204, 205, 206, 207, 208, 203, 204, 205, 206 };
+            int[] brr = { 203, 204, 204, 205, 206, 207, 205, 208, 203, 206, 205, 206, 204 };
             int[] r2 = missingNumbers(arr1, brr);
             displayArray(r2);
+            Console.ReadKey();
 
             // grading students
             Console.WriteLine("\n\nGrading students");
@@ -40,7 +44,7 @@ namespace Assignment2_S19
 
             // find the median
             Console.WriteLine("\n\nFind the median");
-            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3};
+            int[] arr2 = { 0, 1, 2, 4, 6, 5, 3 };
             Console.WriteLine(findMedian(arr2));
 
             // closest numbers
@@ -55,9 +59,11 @@ namespace Assignment2_S19
             Console.WriteLine(dayOfProgrammer(year));
         }
 
-        static void displayArray(int []arr) {
+        static void displayArray(int[] arr)
+        {
             Console.WriteLine();
-            foreach(int n in arr) {
+            foreach (int n in arr)
+            {
                 Console.Write(n + " ");
             }
         }
@@ -65,7 +71,7 @@ namespace Assignment2_S19
         // Complete the rotLeft function below.
         static int[] rotLeft(int[] a, int d)
         {
-            return new int[] {};
+            return new int[] { };
         }
 
         // Complete the maximumToys function below.
@@ -77,15 +83,68 @@ namespace Assignment2_S19
         // Complete the balancedSums function below.
         static string balancedSums(List<int> arr)
         {
-            return "";
+            int[] newarr = new int[arr.Count];
+            arr.CopyTo(newarr);
+            int leftsum = 0;
+            int totalsum = 0;
+            bool flag = false;
+
+            for (int i = 0; i < newarr.Length; i++)
+            {
+                totalsum += newarr[i];
+            }
+            for (int i = 0; i < newarr.Length; i++)
+            {
+
+                totalsum -= arr[i];
+                if (leftsum == totalsum)
+                {
+                    flag = true;
+                    break;
+                }
+                else
+                {
+                    flag = false;
+                }
+                leftsum += arr[i];
+
+            }
+            if (flag == true)
+            {
+                return "YES";
+            }
+            else
+            {
+                return "NO";
+            }
         }
 
         // Complete the missingNumbers function below.
         static int[] missingNumbers(int[] arr, int[] brr)
         {
-            return new int[] { };
-        }
+            int maxbrr = brr.Max();
+            int minbrr = brr.Min();
+            Dictionary<int, int> arrnew = arr.GroupBy(x => x).OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Count());
+            Dictionary<int, int> brrnew = brr.GroupBy(x => x).OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Count());
+            ArrayList missingarr = new ArrayList();
 
+            foreach (var y in brrnew)
+            {
+                if (arrnew.TryGetValue(y.Key, out int value))
+                {
+                    if (value != y.Value)
+                    {
+                        missingarr.Add(y.Key);
+                    }
+                }
+                else
+                {
+                    missingarr.Add(y.Key);
+                }
+            }
+            return missingarr.OfType<int>().ToArray();
+
+        }
 
         // Complete the gradingStudents function below.
         static int[] gradingStudents(int[] grades)
